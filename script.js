0f66d2b804ba97ef785e04b0d51c34b3bfa3ffe4
nsjagtap <115407522+nsjagtap@users.noreply.github.com>
2023-04-23 12:05:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function removebackground() {
+    let images = document.getElementById('urls').value.split('\n')
+
+    images.forEach(imageUrl => {
+        const image = new Image();
+        image.onload = ({ target }) => {
+            const w = Math.round(target.width);
+            const h = Math.round(target.height);
+
+            const canvas = document.createElement("canvas");
+            canvas.width = w;
+            canvas.height = h;
+            const canvasContext = canvas.getContext("2d");
+            canvasContext.drawImage(
+                target,
+                0,
+                0,
+                target.width,
+                target.height,
+                0,
+                0,
+                w,
+                h
+            );
+
+            const canvasImageData = canvasContext.getImageData(0, 0, w, h);
+
+            for (
+                let index = 0, dataLength = canvasImageData.data.length;
+                index < dataLength;
+                index += 4
+            ) {
+                const r = canvasImageData.data[index];
+                const g = canvasImageData.data[index + 1];
+                const b = canvasImageData.data[index + 2];
+                if ([r, g, b].every((item) => item > 230))
+                    canvasImageData.data[index + 3] = 0;
+            }
+
+            target.width = w;
+            target.height = h;
+            canvasContext.putImageData(canvasImageData, 0, 0);
+            document.body.append(image, canvas);
+        };
+        image.crossOrigin = "";
+        image.src = imageUrl;
+    });
+}
